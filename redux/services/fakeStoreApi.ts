9e0ab72f525c265dbd37859d1808005407a95f33
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-10 14:24:46 +0300

    feat: implemented fetch products from fakestore API

---
+++ redux/services/fakeStoreApi.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import { API_ENDPOINTS, BASE_URL } from "@/constants";
+import { Category, Product } from "@/interfaces";
+
+export const fakeStoreApi = createApi({
+  reducerPath: "fakeStoreApi",
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  endpoints: (builder) => ({
+    getCategories: builder.query<Category[], void>({
+      query: () => API_ENDPOINTS.CATEGORIES,
+    }),
+    getProducts: builder.query<Product[], void>({
+      query: () => API_ENDPOINTS.PRODUCTS,
+    }),
+  }),
+});
+
+export const { useGetCategoriesQuery, useLazyGetProductsQuery } = fakeStoreApi;
