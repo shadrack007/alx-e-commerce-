b8ef2451a5cb709bbc565c16c127442c4d082868
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-13 22:27:32 +0300

    fea: implemented card slice

---
+++ redux/features/cart/cartSlice.ts
@@ -0,0 +1,25 @@
+import { CartState, Product } from "@/interfaces";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState: CartState = {
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<Product>) => {
+      state.items.push(action.payload);
+    },
+    removeFromCart: (state, action: PayloadAction<number>) => {
+      state.items = state.items.filter((item) => item.id !== action.payload);
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
