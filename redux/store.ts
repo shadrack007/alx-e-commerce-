1c18f073bea3821521a9fea4baca6fcebdba28cf
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-08-13 22:34:56 +0300

    integrated card reducer

--- redux/store.ts
+++ redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import cartReducer from "./features/cart/cartSlice";
 import { fakeStoreApi } from "./services/fakeStoreApi";
 
 export const store = configureStore({
   reducer: {
     [fakeStoreApi.reducerPath]: fakeStoreApi.reducer,
+    cart: cartReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(fakeStoreApi.middleware),
